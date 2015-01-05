5a735e6d68d86d67e3431ca2ed8fb6024634db1e
Michael Mendenhall <michael.mendenhall@nist.gov>
2015-01-05 08:05:55 -0500

    Indentation cleanup

--- GeneralUtils/ProgressBar.hh
+++ GeneralUtils/ProgressBar.hh
@@ -29,25 +29,25 @@
 /// class for printing a progress bar to stdout
 class ProgressBar {
 public:
-
-	/// constructor
-	ProgressBar(unsigned int nt, unsigned int nm=1, bool v=true, const std::string& label="");
-	
-	/// destructor
-	~ProgressBar() { if(verbose) printf("* Done.\n"); }
-	
-	/// update status at i items completed
-	void update(unsigned int i);
-	
+    
+    /// constructor, given total number of items and number per increment
+    ProgressBar(unsigned int nt, unsigned int nm=1, bool v=true, const std::string& label="");
+    
+    /// destructor
+    ~ProgressBar() { if(verbose) printf("* Done.\n"); }
+    
+    /// update status at i items completed
+    void update(unsigned int i);
+    
 protected:
-
-	const unsigned int ntotal;	///< total number of items to completion
-	const unsigned int nsteps;	///< number of steps to mark = ntotal/nmod
-	const unsigned int nmod;	///< number of items per marked step
-	
-	unsigned int c;				///< number of items completed
-	unsigned int s;				///< steps displayed
-	const bool verbose;			///< whether to display the progress bar
+    
+    const unsigned int ntotal;  ///< total number of items to completion
+    const unsigned int nsteps;  ///< number of steps to mark = ntotal/nmod
+    const unsigned int nmod;    ///< number of items per marked step
+    
+    unsigned int c;             ///< number of items completed
+    unsigned int s;             ///< steps displayed
+    const bool verbose;         ///< whether to display the progress bar
 };
 
 #endif
