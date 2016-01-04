a5a0a256e60e06cd1f2b54ac79acdf14d5018221
Michael Mendenhall <michael.mendenhall@nist.gov>
2016-01-04 16:14:48 -0500

    Threaded RBU clones

--- pyUtils/autologbook/DB_Logger.py
+++ pyUtils/autologbook/DB_Logger.py
@@ -4,20 +4,12 @@
 from sqlite3_RBU import *
 import time
 
-class DB_Logger:
-    """Base class for writing data log"""
-
-    def __init__(self, dbname):
-        """Initialize with name of database to open"""
-        self.conn = sqlite3.connect(dbname)
-        self.curs = self.conn.cursor()
-
-    def __del__(self):
-        """Close DB connection on deletion"""
-        if self.conn:
-            self.conn.commit()
-            self.conn.close()
-            self.conn = None
+class DB_Reader:
+    """Base class for reading DB file"""
+    def __init__(self, dbname, conn = None):
+            """Initialize with name of database to open; defaults to read-only if conn not supplied"""
+            self.conn = conn if conn is not None else sqlite3.connect("file:%s?mode=ro"%dbname, uri=True)
+            self.curs = self.conn.cursor()
     
     def get_inst_type(self, name):
         """Get instrument identifier by name"""
@@ -33,6 +25,21 @@ class DB_Logger:
             self.curs.execute("SELECT readout_types.rowid FROM readout_types JOIN instrument_types ON instrument_id = instrument_types.rowid WHERE readout_types.name = ? AND instrument_types.name = ?", (name, inst_name))
         r = self.curs.fetchall()
         return r[0][0] if len(r) == 1 else None 
+    
+class DB_Logger(DB_Reader, RBU_cloner):
+    """Base class for writing data log"""
+
+    def __init__(self, dbname, conn = None):
+        """Initialize with name of database to open"""
+        DB_Reader.__init__(self, None, conn if conn is not None else sqlite3.connect(dbname))
+        RBU_cloner.__init__(self, self.conn.cursor())
+
+    def __del__(self):
+        """Close DB connection on deletion"""
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
+            self.conn = None
             
     def create_instrument(self, nm, descrip, devnm, sn, overwrite = False):
         """Assure instrument entry exists, creating/updating as needed"""
@@ -52,8 +59,13 @@ class DB_Logger:
         """Log reading, using current time for timestamp if not specified"""
         if t is None:
             t = time.time()
-        self.curs.execute("INSERT INTO readings(type_id, time, value) VALUES (?,?,?)", (tid,value,t))
-    
+        self.insert("readings", {"type_id":tid, "time":t, "value":value})
+        self.log_readout_hook(tid, value, t)
+        
+    def log_readout_hook(self, tid, value, t):
+        """Hook for subclass to check readout values"""
+        pass
+
 if __name__=="__main__":
     D = DB_Logger("test.db")
     D.create_instrument("funcgen", "test function generator", "ACME Foobar1000", "0001")
@@ -62,8 +74,12 @@ if __name__=="__main__":
     D.conn.commit()
     
     from math import *
+    t0 = 0
     while 1:
         t = time.time()
+        if t > t0 + 10:
+            D.restart_stuffer()
+            t0 = t
         v0 = sin(2*pi*t/300)
         v1 = sin(2*pi*t/(12*3600))
         D.log_readout(r0, v0, t)
--- pyUtils/autologbook/README.txt
+++ pyUtils/autologbook/README.txt
@@ -2,26 +2,21 @@ autologbook lab logging utility suite
 Michael P. Mendenhall (2016)
 
 a collection of Python scripts for generating and managing sqlite3 database log files of lab "slow control" readings
+INCOMPLETE WORK IN PROGRESS
+MANY COMPONENTS MISSING
 
+DB_Logger:
+    - reads in datapoints from devices
+    - writes to logbook DB file, copied to differential changes RBU files
+    - checks for "actionable" conditions (out-of-range values, etc.)
+    - manages rollover/archiving of logbook DB files
 
-Logger:
-	- reads in datapoints from devices
-	- writes to logbook DB file, possible remote copies
-	- checks for "actionable" conditions (out-of-range values, etc.)
-	- manages rollover/archiving of logbook DB files
-	- needs to be fully fault tolerant
+DB_Server:
+    - pulls RBU data over network for "reasonably up-to-date" response
+    - handles data I/O from multiple files
+    - in-memory cache of recent data points
+    - generate webpage summary tables, plots
 
 Locator:
-	- interface for locating and collecting archived data for analysis tasks
-
-Plotter:
-	- locates applicable files
-	- in-memory data caches, incrementally updated (minimize thrashing of DB file)
-	- receives plot requests
-
-Display:
-	- web interface or local "live" display
-
-dynamic mode: webpage is generated for each request
-cached mode: webpage is cached, re-generated on request
-static: non-interactive webpage is pre-generated at regular intervals
+    - interface for locating and collecting archived data for analysis tasks
+    - another place to put trigger actions
