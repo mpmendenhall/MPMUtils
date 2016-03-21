8ae87f40c0e0af410dad2db37b1abf04b095ee4c
Michael Mendenhall <michael.mendenhall@nist.gov>
2016-03-21 13:23:22 -0400

    Linkdef for TCumulativeMap<string, Double_t>

--- ROOTUtils/LinkDef.h
+++ ROOTUtils/LinkDef.h
@@ -5,6 +5,7 @@
 #pragma link C++ class CumulativeData+;
 #pragma link C++ class TCumulative+;
 #pragma link C++ class TCumulativeMap<Int_t, Double_t>+;
+#pragma link C++ class TCumulativeMap<string, Double_t>+;
 #pragma link C++ class TDynamicHistogram+;
 #pragma link C++ class TDynamicLogHistogram+;
 #pragma link C++ class TRateCategories+;
