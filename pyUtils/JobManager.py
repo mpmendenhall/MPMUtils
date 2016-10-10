1d256869b716673fdea68fba5966427f5ef84a51
Michael Mendenhall <mpmendenhall@llnl.gov>
2016-10-10 12:56:49 -0700

    Show status names instead of numbers

--- pyUtils/JobManager.py
+++ pyUtils/JobManager.py
@@ -7,6 +7,8 @@ from optparse import OptionParser
 from time import sleep
 
 jobstates = {"Idle":2,"Starting":3,"Running":3,"Completed":4,"Unknown":5,"Removed":6,"Bundled":7}
+statenames = {-1:"hold", 0:"waiting", 1:"sumitted", 2:"queued", 3:"running", 4: "done" }
+def statename(i): return statenames.get(i,"status %i"%i)
 
 def get_showq_runstatus():
     """Get queue status according to showq"""
@@ -28,7 +30,7 @@ def summarize_DB_runstatus(curs):
     for i in range(11):
         curs.execute("SELECT COUNT(*) FROM jobs WHERE status=?",(i,))
         n = curs.fetchone()[0]
-        if n: print("\tStatus %i: %i jobs"%(i,n))
+        if n: print("\t%s: %i jobs"%(statename(i),n))
 
 def checkjob(jid):
     """Get info from checkjob"""
@@ -46,16 +48,16 @@ def checkjob(jid):
 def update_DB_runstatus(curs,jstatus):
     """Update run status in database from job status list"""
     curs.executemany("UPDATE jobs SET status=? WHERE queue_id=?",[(j[1],j[0]) for j in jstatus])
-    
+
     jknown = frozenset([j[0] for j in jstatus if 1 <= j[1] <= 3])
     curs.execute("SELECT queue_id FROM jobs WHERE 1 <= status AND (status <= 3 OR status == 5)")
     missingjobs = [(j[0],checkjob(j[0])) for j in curs.fetchall() if j[0] not in jknown]
     missingjobs = [(j[1]["status"],j[1].get("ret_code",None),j[1].get("walltime",None),j[0]) for j in missingjobs]
     if missingjobs: curs.executemany("UPDATE jobs SET status=?,return_code=?,use_walltime=? WHERE queue_id=?",missingjobs)
-    
+
 def msub_job(curs, jid, qsettings, mcmds=["-j oe", "-V"]):
     """Submit job via msub; update DB"""
-    
+
     curs.execute("SELECT name,jobfile,outlog,n_nodes,est_walltime FROM jobs WHERE job_id = ?",(jid,))
     j = curs.fetchone()
     if not j: return
@@ -63,7 +65,7 @@ def msub_job(curs, jid, qsettings, mcmds=["-j oe", "-V"]):
     # modify job script with msub commands
     jobin = open(j[1],"r").read()
     jobout = open(j[1],"w")
-    
+
     jobout.write("#MSUB -A %s\n"%qsettings["account"])
     if j[2]: jobout.write("#MSUB -o %s\n"%j[2])
     if j[0]: jobout.write("#MSUB -N %s\n"%j[0])
@@ -71,10 +73,10 @@ def msub_job(curs, jid, qsettings, mcmds=["-j oe", "-V"]):
     jobout.write("#MSUB -l nodes=%i\n"%j[3])
     jobout.write("#MSUB -l walltime=%i\n"%j[4])
     for m in mcmds: jobout.write("#MSUB "+m+"\n")
-    
+
     jobout.write(jobin)
     jobout.close()
-    
+
     o = subprocess.getoutput('msub "%s"'%j[1]).strip()
     print("Job '%s' submitted as '%s'"%(j[0],o))
     qid = int(o.split(".")[0])
@@ -98,7 +100,7 @@ def update(conn):
     conn.execute('BEGIN EXCLUSIVE')
     update_DB_runstatus(curs,jstatus)
     conn.commit()
-    
+
 def update_and_launch(conn,qsettings):
     """Update status; launch new jobs as available"""
     jstatus = get_showq_runstatus()
@@ -106,9 +108,9 @@ def update_and_launch(conn,qsettings):
     for j in jstatus:
         if j[1] <= 3: ncpu += j[2]
     print("Found reservations for %i cpu cores"%ncpu)
-    
+
     curs = conn.cursor()
-    
+
     conn.isolation_level = 'EXCLUSIVE'
     conn.execute('BEGIN EXCLUSIVE')
 
@@ -117,7 +119,7 @@ def update_and_launch(conn,qsettings):
     if ncpu < qsettings["limit"]:
         jnext = get_nmore(curs,qsettings["limit"]-ncpu)
         print("Submitting %i new jobs."%len(jnext))
-        for j in jnext: 
+        for j in jnext:
             msub_job(curs,j,qsettings)
             conn.commit()
 
@@ -126,7 +128,7 @@ def update_and_launch(conn,qsettings):
 def cancel_queued_jobs(conn):
     """Force cancel all queued up jobs"""
     update(conn)
-    
+
     curs = conn.cursor()
     curs.execute("SELECT queue_id FROM jobs WHERE queue_id > 0 AND status <= 2")
     for j in curs.fetchall():
@@ -134,14 +136,14 @@ def cancel_queued_jobs(conn):
     curs.execute("UPDATE jobs SET status = 6 WHERE status = 0")
     conn.commit()
     summarize_DB_runstatus(curs)
-    
+
 
 def load_test_batch(qsettings, njobs=8):
     """Generate test run batch"""
     home = os.environ["HOME"]
     for i in range(njobs):
         open(home+"/test_%i.sh"%i,"w").write('echo "Hello world %i!"\nsleep 30\necho "Goodbye!"'%i)
-    
+
     conn = sqlite3.connect(qsettings["db"])
     curs = conn.cursor()
     with conn:
@@ -152,7 +154,7 @@ def upload_onejob(curs, name, infile, logfile, nnodes, walltime):
     """Upload one job to run; return DB job identifier"""
     curs.execute("INSERT INTO jobs(name,jobfile,outlog,n_nodes,est_walltime,status) VALUES (?,?,?,?,?,0)", (name, infile, logfile, nnodes, walltime))
     return curs.lastrowid
-    
+
 def upload_jobs(curs,joblist):
     """Upload a list of jobs to run"""
     # job specifier: (name,input,log,nodes,walltime)
@@ -201,7 +203,7 @@ def make_bundle_jobs(curs,nnodes,jname,bundledir,jlist,tmax,nmax):
         bjobfile.close()
         bundle_id = upload_onejob(curs, "bundle_"+jname, bfname, bfname+"_log.txt", nnodes, tsum)
         for i in jb: curs.execute("UPDATE jobs SET status=7,associated=? WHERE job_id=?",(bundle_id,jlist[i][0]))
-        
+
 def rebundle(curs,jname,bundledir,tmax,nmax=1000):
     """Bundle small jobs together into longer-time units"""
     curs.execute("SELECT job_id,jobfile,outlog,n_nodes,est_walltime FROM jobs WHERE status=0 AND name=? AND est_walltime < ?",(jname,tmax))
@@ -225,18 +227,18 @@ def cycle_launcher(conn,qsettings,twait=60):
 ###################
 if __name__ == "__main__":
     parser = OptionParser()
-    
+
     parser.add_option("--account",  help="submission user account")
     parser.add_option("--queue",  help="submission queue")
     parser.add_option("--limit",  type="int", default=10, help="concurrent jobs limit")
     parser.add_option("--db",  help="jobs database")
-    
+
     parser.add_option("--launch", action="store_true", help="update and launch")
     parser.add_option("--cycle", action="store_true", help="continuously re-check jobs")
     parser.add_option("--status", action="store_true", help="update and display status")
     parser.add_option("--cancel", action="store_true", help="cancel queued jobs")
     parser.add_option("--clear", action="store_true", help="clear completed jobs")
-    
+
     options, args = parser.parse_args()
 
     qs = {"account":options.account, "queue":options.queue, "limit":options.limit, "db":options.db}
@@ -248,5 +250,5 @@ if __name__ == "__main__":
     if options.cancel: cancel_queued_jobs(conn)
     if options.clear: clear_completed(conn)
     if options.cycle: cycle_launcher(conn,qs)
-    
+
     conn.close()
