90d9ba6452e0037ff2cfb18ab277a3044166aa21
Michael Mendenhall <mendenhall5@llnl.gov>
2019-03-29 08:18:57 -0700

    Add missing files

---
+++ Utility/PointSelector.cc
@@ -0,0 +1,77 @@
+/// \file PointSelector.cc
+
+#include "PointSelector.hh"
+#include "DiskBIO.hh"
+
+void PointSelector::display() const {
+    printf("PointSelector for %zu dimensions in %zu partitions:\n", Ntot, parts.size());
+    for(auto& p: parts) printf("\t%zu dimensions: %zu x %zu points (at %zu)\n", p.N, p.npts, p.Nsub, p.QRNGn);
+}
+
+void PointSelector::addPart(size_t N, size_t npts) {
+    if(!N || !npts) return;
+    for(auto& p: parts) p.Nsub *= npts;
+    parts.emplace_back(N,npts);
+    v0.resize(v0.size() + N);
+    Ntot += N;
+    assert(Ntot == v0.size());
+}
+
+void PointSelector::skipTo(size_t i) {
+    for(auto it = parts.rbegin(); it != parts.rend(); ++it) {
+        auto j = i / it->Nsub;
+        assert(j <= it->QRNGn);
+        it->QRNG.Skip(j - it->QRNGn);
+    }
+}
+
+PointSelector::vec_t PointSelector::next() {
+    size_t i = v0.size();
+    subgroup = parts.size();
+    for(auto it = parts.rbegin(); it != parts.rend(); ++it) {
+        i -= it->N;
+        it->QRNG.Next(v0.data()+i);
+        --subgroup;
+        if(it->QRNGn++ % it->npts) break;
+    }
+    return v0;
+}
+
+template<>
+void BinaryIO::send<PointSelector::axpart>(const PointSelector::axpart& p) {
+    send(p.N);
+    send(p.npts);
+    send(p.Nsub);
+    send(p.QRNGn);
+}
+
+template<>
+void BinaryIO::receive<PointSelector::axpart>(PointSelector::axpart& p) {
+    receive(p.N);
+    receive(p.npts);
+    receive(p.Nsub);
+    receive(p.QRNGn);
+    p.QRNG = ROOT::Math::QuasiRandomNiederreiter(p.N);
+    p.QRNG.Skip(p.QRNGn);
+}
+
+std::ostream& operator<<(std::ostream& o, const PointSelector& p) {
+    IOStreamBIO b(nullptr, &o);
+    b.start_wtx();
+    b.send(p.subgroup);
+    b.send(p.parts);
+    b.send(p.Ntot);
+    b.send(p.v0);
+    b.end_wtx();
+
+    return o;
+}
+
+std::istream& operator>> (std::istream &i, PointSelector& p) {
+    IOStreamBIO b(&i, nullptr);
+    b.receive(p.subgroup);
+    b.receive(p.parts);
+    b.receive(p.Ntot);
+    b.receive(p.v0);
+    return i;
+}
---
+++ Utility/PointSelector.hh
@@ -0,0 +1,59 @@
+/// \file "PointSelector.hh" Multidimensional hierarchical point generator
+
+#ifndef POINTSELECTOR_HH
+#define POINTSELECTOR_HH
+
+#include <Math/QuasiRandom.h>
+#include <iostream>
+#include <vector>
+using std::vector;
+
+/// Multidimensional hierarchical point selection
+class PointSelector {
+public:
+    typedef vector<double> vec_t;   ///< convenience shorthand
+
+    /// Add partitioned subgroup of N elements, to be sampled npts times
+    void addPart(size_t N, size_t npts);
+    /// skip to enumerated coordinate
+    void skipTo(size_t i);
+    /// generate next coordinate
+    vec_t next();
+    /// print debugging info to stdout
+    void display() const;
+    /// get number of points in full cycle
+    size_t nCycle() const { return parts.size()? parts[0].npts * parts[0].Nsub : 0; }
+
+    size_t subgroup;        ///< indicates modified subgroup on next()
+
+    /// partitioning of fit axes
+    struct axpart {
+        /// Default constructor
+        axpart(): N(0), npts(0) { }
+        /// Constructor, with number of elements
+        axpart(size_t _N, size_t _npts): N(_N), npts(_npts), QRNG(N) { }
+
+        size_t N;           ///< number of items on this axis
+        size_t npts;        ///< number of points to generate at this level
+        size_t Nsub = 1;    ///< number of points for sub-groupings
+
+        ROOT::Math::QuasiRandomNiederreiter QRNG;   ///< quasirandom distribution generator
+        size_t QRNGn = 0;                           ///< number of points pulled from QRNG
+    };
+
+protected:
+
+    vector<axpart> parts;   ///< partioning of N for sub-calculations
+    size_t Ntot = 0;        ///< total number of dimensions
+    vec_t v0;               ///< previously-generated point
+
+    friend std::ostream& operator<< (std::ostream &o, const PointSelector& NM);
+    friend std::istream& operator>> (std::istream &i, PointSelector& NM);
+};
+
+/// serialize to output
+std::ostream& operator<<(std::ostream& o, const PointSelector& p);
+/// deserialize (requires correct dimension to already be set)
+std::istream& operator>>(std::istream& i, PointSelector& p);
+
+#endif
