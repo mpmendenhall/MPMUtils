3cf7f54e53df532b598c19cab2b60f94f5a95788
Michael Mendenhall <mendenhall5@llnl.gov>
2019-09-04 08:39:15 -0700

    Migrate file

---
+++ Math/DiskQuadrature.hh
@@ -0,0 +1,23 @@
+/// DiskQuadrature.hh Quadrature integration rules for disk
+// Abramowitz and Stegun 25.4.61 (p. 891--)
+
+// x, y, weight, for disk radius = 0.5
+
+const double diskquad_1[][3] = { {0.,0.,1.} };
+
+const double diskquad_4[][3] = {
+    { .25,  .25, .25},
+    { .25, -.25, .25},
+    {-.25,  .25, .25},
+    {-.25, -.25, .25} };
+
+const double diskquad_9[][3] = {
+    {  0,    0,  1/6.},
+    { .25,  .25, 1/6.},
+    { .25, -.25, 1/6.},
+    {-.25,  .25, 1/6.},
+    {-.25, -.25, 1/6.},
+    { .5,    0,  1/24.},
+    {-.5,    0,  1/24.},
+    {  0,   .5,  1/24.},
+    {  0,  -.5,  1/24.} };
