49c10fb93141c40917303c3590b0d6a7eabf4325
Michael Mendenhall <mendenhall5@llnl.gov>
2019-10-24 14:49:10 -0700

    Improved thermal n calcs

--- Physics/RefDataDB/ENDF_nThermal.py
+++ Physics/RefDataDB/ENDF_nThermal.py
@@ -8,46 +8,57 @@ from math import *
 m_n = 939.5654133e6 # CODATA 2014 neutron mass, eV
 k_B = 8.6173303e-5  # CODATA 2014 Boltmann constant, eV/K
 
-def ncapt_thermal(EDB, Z, A, T = 300):
+class ncapt_thermal:
     """1/v-region thermal neutron capture cross section estimate from ENDF data"""
+    def __init__(self, EDB, Z, A, T = 300):
 
-    print("Z =", Z, "\tA =", A)
+        self.Z = Z
+        self.A = A
 
-    # neutron characteristic velocity at temperature [c]
-    vc = sqrt(pi*k_B*T/(2*m_n))
-    Ec = 0.25*pi*k_B*T
+        # neutron characteristic velocity at temperature [c]
+        self.vc = sqrt(pi*k_B*T/(2*m_n))
+        self.Ec = 0.25*pi*k_B*T
 
-    # total interaction cross section
-    sTot = EDB.find_sections({"A": A, "Z": Z, "MF": 3, "MT": 1})
-    if not sTot: return None
-    sTot = EDB.get_section(sTot[0])
-    #print(sTot)
+        # total interaction cross section
+        sTot = EDB.find_sections({"A": A, "Z": Z, "MF": 3, "MT": 1})
+        if not sTot:
+            self.s = None
+            return
+        sTot = EDB.get_section(sTot[0])
 
-    #E0 = sTot.xs[0]
-    #s0 = sTot.ys[0]
-    E0 = Ec
-    s0 = sTot(E0)
+        #E0 = sTot.xs[0]
+        #s0 = sTot.ys[0]
+        E0 = self.Ec
+        s0 = sTot(E0)
 
-    # neutron velocity at measured point [c]
-    v0 = sqrt(2*E0/m_n)
+        # neutron velocity at measured point [c]
+        v0 = sqrt(2*E0/m_n)
 
-    # subtract any elastic scattering component
-    sEl = EDB.find_sections({"A": A, "Z": Z, "MF": 3, "MT": 2})
-    el0 = EDB.get_section(sEl[0])(E0) if sEl else 0
+        # Non-elastic directly available?
+        sNEl = EDB.find_sections({"A": A, "Z": Z, "MF": 3, "MT": 3})
+        nel0 = EDB.get_section(sNEl[0])(E0) if sNEl else None
 
-    print("Total cross section", s0, "at E =", E0, "MeV")
-    if el0:
-        print("\tof which", el0, "is elastic scattering")
-        s0 -= el0
+        if nel0:
+            #print("\tof which", nel0, "is inelastic")
+            s0 = nel0
+        else:
+            # subtract elastic scattering if available
+            sEl = EDB.find_sections({"A": A, "Z": Z, "MF": 3, "MT": 2})
+            el0 = EDB.get_section(sEl[0])(E0) if sEl else None
+            if el0:
+                #print("\tof which", el0, "is elastic scattering")
+                s0 -= el0
 
-    # thermal-averaged cross section
-    s = s0*v0/vc
+        # thermal-averaged cross section
+        self.s = s0*v0/self.vc
 
-    print("v * sigma =",  vc * 299792458, "[m/s] *", s, "[bn]")
-    return s
+    def display(self):
+        print("Z =", self.Z, "\tA =", self.A)
+        print("v * sigma =",  self.vc * 299792458, "[m/s] *", self.s, "[b]")
 
 if __name__ == "__main__":
     EDB = ENDFDB()
-    ncapt_thermal(EDB, 1, 1)
-    ncapt_thermal(EDB, 3, 6)
-    ncapt_thermal(EDB, 17, 35)
+    ncapt_thermal(EDB, 1, 1).display()
+    ncapt_thermal(EDB, 3, 6).display()
+    ncapt_thermal(EDB, 8, 16).display()
+    ncapt_thermal(EDB, 17, 35).display()
