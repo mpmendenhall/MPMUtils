a27cd3a68570b7fcec9caab301eb63df5d4affbe
Michael Mendenhall <michael@mendenhall.science>
2020-11-04 10:50:00 -0800

    Sync updates

--- CXXWarnings.cmake
+++ CXXWarnings.cmake
@@ -1,5 +1,9 @@
-
-LIST(APPEND CXXOPTS "-Wall" "-Wextra" "-Werror")
+LIST(APPEND CXXOPTS "-Wall" "-Wextra")
+option(WERROR "Enable/disable -Werror compile flags" ON)
+IF(WERROR)
+    MESSAGE(STATUS "Enabling -Werror compile flag, turning warnings into errors")
+    LIST(APPEND CXXOPTS "-Werror")
+endif()
 
 IF(CMAKE_COMPILER_IS_GNUCC)
     LIST(APPEND CXXOPTS "-Wpedantic" "-Wlogical-op" "-Wshadow" "-Wformat=2" "-pedantic-errors")
@@ -9,7 +13,7 @@ IF(CMAKE_COMPILER_IS_GNUCC)
         LIST(APPEND CXXOPTS "-Wuseless-cast")
     else()
         LIST(APPEND CXXOPTS "-Wduplicated-cond" "-Wmissing-include-dirs" "-Wredundant-decls"
-        "-Wnull-dereference" "-Wsuggest-override")
+        "-Wnull-dereference" "-Wsuggest-override" "-Wuninitialized" "-Winit-self")
 
         if(NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS "7.0")
             LIST(APPEND CXXOPTS "-Wduplicated-branches" "-Woverloaded-virtual")
