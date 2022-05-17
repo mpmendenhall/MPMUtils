908d6d26deaa9cdc9049f59e5b5df5dd15dfe90a
Michael Mendenhall <michael@mendenhall.science>
2022-05-17 16:57:32 -0700

    More elements

--- Physics/PDGid.cc
+++ Physics/PDGid.cc
@@ -3,25 +3,49 @@
 #include "PDGid.hh"
 #include "to_str.hh"
 
+const string& elSymb(unsigned int Z) {
+    const size_t maxel = 119;
+    static const string elnames[maxel] = {       "n",
+        "H",                                                                                    "He",
+        "Li","Be",                                                     "B", "C", "N", "O", "F", "Ne",
+        "Na","Mg",                                                     "Al","Si","P", "S", "Cl","Ar",
+        "K", "Ca","Sc",   "Ti","V", "Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
+        "Rb","Sr","Y",    "Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I", "Xe",
+        "Cs","Ba","La",
+                       "Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+                          "Hf","Ta","W", "Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
+        "Fr","Ra","Ac",
+                       "Th","Pa","U", "Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr",
+                          "Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"
+    };
+    static const string whoknows = "!";
+    return Z < maxel? elnames[Z] : whoknows;
+}
+
 string isot_name_ZA(unsigned int Z, unsigned int A) {
-    const size_t maxel = 19;
-    static const char* const elnames[maxel+1] = {"?","H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K"};
     if(A==1 && Z==0) return "n";
-    return (Z<=maxel)? to_str(A)+elnames[Z] : "A"+to_str(A)+"Z"+to_str(Z);
+    return to_str(A)+elSymb(Z);
 }
 
 string PDG_PID_name(PDGid_t i) {
     switch(i) {
-        case PDG_ALPHA:    return "alpha";
-        case PDG_TRITON:   return "triton";
-        case PDG_ELECTRON: return "e-";
-        case PDG_POSITRON: return "e+";
-        case PDG_NUEBAR:   return "antinu_e";
-        case PDG_MUMINUS:  return "mu-";
-        case PDG_MUPLUS:   return "mu+";
-        case PDG_GAMMA:    return "gamma";
-        case PDG_NEUTRON:  return "n";
-        case PDG_PROTON:   return "p";
+        case PDG_X:         return "???";
+        case PDG_ALPHA:     return "alpha";
+        case PDG_TRITON:    return "triton";
+        case PDG_ELECTRON:  return "e-";
+        case PDG_POSITRON:  return "e+";
+        case PDG_NUEBAR:    return "antinu_e";
+        case PDG_MUMINUS:   return "mu-";
+        case PDG_MUPLUS:    return "mu+";
+        case PDG_GAMMA:     return "gamma";
+        case PDG_NEUTRON:   return "n";
+        case PDG_PROTON:    return "p";
+        case PDG_PI0:       return "pi0";
+        case PDG_PIPLUS:    return "pi+";
+        case PDG_PIMINUS:   return "pi-";
+        case PDG_K0:        return "K0";
+        case PDG_KPLUS:     return "K+";
+        case PDG_KMINUS:    return "K-";
         default: break;
     }
 
--- Physics/PDGid.hh
+++ Physics/PDGid.hh
@@ -9,25 +9,35 @@ using std::string;
 #include <inttypes.h>
 
 /// PDG / Geant4 particle type identifiers
-/// Special IDs for particles without PDG encoding
-/// using reserved "For MC internal use" range 81--100
 enum PDGid_t: int32_t {
-    PDG_X        = 0,
-    PDG_ELECTRON = 11,
-    PDG_POSITRON = -11,
-    PDG_NUEBAR   = -12,
-    PDG_MUMINUS  = 13,
-    PDG_MUPLUS   = -13,
-    PDG_GAMMA    = 22,
-    PDG_NEUTRON  = 2112,
-    PDG_PROTON   = 2212,
+    PDG_X           = 0,
+    PDG_ELECTRON    = 11,
+    PDG_POSITRON    = -PDG_ELECTRON,
+    PDG_NUE         = 12,
+    PDG_NUEBAR      = -PDG_NUE,
+    PDG_MUMINUS     = 13,
+    PDG_MUPLUS      = -PDG_MUMINUS,
+    PDG_GAMMA       = 22,
+    PDG_PI0         = 111,
+    PDG_PIPLUS      = 211,
+    PDG_PIMINUS     = -PDG_PIPLUS,
+    PDG_K0          = 311,
+    PDG_KPLUS       = 321,
+    PDG_KMINUS      = -PDG_KPLUS,
+    PDG_NEUTRON     = 2112,
+    PDG_PROTON      = 2212,
+    // reserved "For MC internal use" range 81--100
     PDG_OPTICALPHOTON = 81,
-    PDG_ION_A    = 10,
-    PDG_ION_Z    = 10000,
-    PDG_ION      = 1000000000,
-    PDG_TRITON   = PDG_ION + PDG_ION_Z + 3*PDG_ION_A,
-    PDG_ALPHA    = PDG_ION + 2*PDG_ION_Z + 4*PDG_ION_A
+    // ions
+    PDG_ION_A       =         10,
+    PDG_ION_Z       =      10000,
+    PDG_ION         = 1000000000,
+    PDG_TRITON      = PDG_ION + PDG_ION_Z + 3*PDG_ION_A,
+    PDG_ALPHA       = PDG_ION + 2*PDG_ION_Z + 4*PDG_ION_A
 };
+
+/// element symbols; "n" for Z=0, "!" for Z > 118
+const string& elSymb(unsigned int Z);
 
 /// identifier for an ion
 constexpr PDGid_t PDG_IonZA(int Z, int A, int L = 0) { return PDGid_t(PDG_ION + PDG_ION_Z*Z + PDG_ION_A*A + L); }
