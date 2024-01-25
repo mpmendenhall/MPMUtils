5ca425e24adf13694b5c7ef786296139891ff11e
Michael P. Mendenhall <mpmendenhall@llnl.gov>
2024-01-25 13:54:02 -0500

    Sync bugfix update

--- Utility/LocklessCircleBuffer.hh
+++ Utility/LocklessCircleBuffer.hh
@@ -13,7 +13,7 @@ template<typename T>
 class LocklessCircleBuffer: public Threadworker {
 public:
     /// Constructor
-    LocklessCircleBuffer(size_t n = 1024): buf(n), ready(n) { }
+    LocklessCircleBuffer(size_t n = 1024) { allocate(n); }
 
     /// change buffer size
     virtual void allocate(size_t n) {
@@ -31,20 +31,25 @@ public:
     }
 
     /// get pointer to next buffer space, with timeout in s; nullptr if unavailable
-    T* get_writepoint(double t_s) {
+    T* get_writepoint(double t_s, bool fail_OK = true) {
         if(writept) throw std::logic_error("Unfinished write in progress");
 
-        if(!t_s) {
+        if(!t_s) { // immediate return without timeout
+            // does write point have a pending read?
             if(!ready[write_idx]) writept = &buf[write_idx];
         } else {
             auto t = std::chrono::steady_clock::now() + std::chrono::milliseconds(int(1e3*t_s));
             do {
+                // does write point have a pending read?
                 if(ready[write_idx]) { usleep(1000); continue; }
                 writept = &buf[write_idx];
             } while(!writept && std::chrono::steady_clock::now() < t);
         }
 
-        if(!writept) ++n_write_fails;
+        if(!writept) {
+            if(!fail_OK) throw std::logic_error("Timeout waiting for write point");
+            ++n_write_fails;
+        }
         return writept;
     }
 
@@ -52,10 +57,10 @@ public:
     void finish_write() {
         if(!writept) throw std::logic_error("No write in progress");
         __sync_synchronize();
-        ready[write_idx] = true;
-        write_idx = (write_idx + 1)%buf.size();
+        ready[write_idx] = true; // mark as written
+        write_idx = (write_idx + 1) % buf.size();
         writept = nullptr;
-        inputReady.notify_one();
+        inputReady.notify_one(); // notify new read available
     }
 
     /// write to next buffer space, failing if unavailable
@@ -72,25 +77,37 @@ public:
         if(!ready[read_idx]) return false;
         current = buf[read_idx];
         __sync_synchronize();
-        ready[read_idx] = false;
         process_item();
+        auto r0 = read_idx;
         read_idx = (read_idx + 1)%buf.size();
+        ready[r0] = false;
         return true;
     }
 
     /// consume all next available items
     size_t flush() {
         int nread = 0;
-        while(read_one()) nread++;
+        while(read_one()) ++nread;
         return nread;
     }
 
     /// count number of buffered items... not guaranteed correct
     size_t n_buffered() const {
-        size_t iw = write_idx;
-        size_t ir = read_idx;
-        if(ir > iw) iw += buf.size();
-        return iw==ir? 0 : iw - ir - 1;
+        auto iw = write_idx;
+        auto ir = read_idx;
+        if(iw < ir) iw += buf.size();
+        return iw==ir? (ready[ir]? buf.size() : 0) : iw - ir - 1;
+    }
+
+    /// wait for buffer clear to fraction, with timeout (s) and optional error
+    void wait_buffer(double timeout, double frac = 0.2, double fail_OK = true) const {
+        size_t targ = frac * buf.size();
+        auto t = std::chrono::steady_clock::now() + std::chrono::milliseconds(int(1e3*timeout));
+        while(std::chrono::steady_clock::now() < t) {
+            if(n_buffered() <= targ) return;
+            usleep(1000);
+        }
+        if(!fail_OK) throw std::logic_error("Timeout waiting for buffer clear");
     }
 
     /// task to be run in thread
@@ -112,9 +129,9 @@ public:
 
 protected:
     vector<T> buf;          ///< data buffer
-    vector<int> ready;      ///< read indicator; int to encourage atomic updating
+    vector<int> ready;      ///< ready-to-read indicator for each buffer item; int to encourage atomic updating
     T* writept = nullptr;   ///< current item being modified
-    T current;              ///< current item to process
+    T current;              ///< current item to process reading
     size_t write_idx = 0;   ///< write point marker
     size_t read_idx = 0;    ///< read point marker
 };
